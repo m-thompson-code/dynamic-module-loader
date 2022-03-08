aca7eb43d33a5c7d2f81e350b678c707b286e047
Mark Thompson <mthompson@bitovi.com>
2022-03-07 19:40:45 -0800

    Auto-generated commit [ci skip]

--- src/app/services/feature-flag/feature-flag.model.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from "@angular/router";
-
-export abstract class FeatureFlagFactory {
-  abstract getFeatureRoutes(): Routes
-}
-
-export type GetRoutesFactory = (routes: Routes) => (featureFlagFactory: FeatureFlagFactory) => Routes
